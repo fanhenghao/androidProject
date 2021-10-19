c793e3bf1444ee7b3b63b39c774a8fd945c7507f
fanhenghao <95024708@qq.com>
2021-10-19 09:51:11 +0800

    修复调用系统相机崩溃问题

--- app/src/main/java/com/fhh/technology/module/home/headicon/HeadPortraitActivity.java
+++ app/src/main/java/com/fhh/technology/module/home/headicon/HeadPortraitActivity.java
@@ -150,7 +150,7 @@ public class HeadPortraitActivity extends BaseActivity implements HeadPortraitCo
                 File.separator + System.currentTimeMillis() + ".jpeg";
 
         mImaegeUri = FileProvider.getUriForFile(mActivity,
-               "com.fhh.technology.provider",
+               "com.fhh.technology.fileprovider",
                 new File(mPath));
         intoPhotograph.putExtra(MediaStore.EXTRA_OUTPUT, mImaegeUri);
         // 授予目录临时共享权限
